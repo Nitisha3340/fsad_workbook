c7506a352bb8f9c66e81f8704318bc4ac05b5d6d
Nitisha Mishra <2400033340@kluniversity.in>
2026-03-21 11:41:08 +0530

    Delete src directory

--- src/main/java/com/klu/course/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.klu.course.repository;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.klu.course.entity.Course;
-
-public interface CourseRepository extends JpaRepository<Course, Integer> {
-    List<Course> findByTitleContaining(String title);
-}
